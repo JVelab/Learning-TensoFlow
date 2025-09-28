0a232c2c7b1f2151dafd2de22c240fdadfa35968
JVelab <jlvela94@gmail.com>
2025-09-28 16:26:11 +0200

    First Commit

---
+++ primer_tensorflow.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+# Crear un tensor simple
+x = tf.constant([1.0, 2.0, 3.0])
+print("Tensor inicial:", x)
+
+# Operación básica: multiplicar por 2
+y = x * 2
+print("Tensor multiplicado por 2:", y)
